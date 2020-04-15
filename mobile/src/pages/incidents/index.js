59c0ade3d408db59006016030d79c9d71f8ce5ed
tiagob85 <tiago.gpes@gmail.com>
2020-04-15 18:10:47 -0300

    Implementação do mobile do projeto Be The Hero completa.

--- mobile/src/pages/incidents/index.js
+++ mobile/src/pages/incidents/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import {Feather} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
+import {View, Image, Text, TouchableOpacity, FlatList} from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
 
 export default function Incidents(){
+    const navigation = useNavigation();
+
+    function navigateToDetail(){
+        navigation.navigate('Detail');
+    }
+
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
@@ -17,6 +26,33 @@ export default function Incidents(){
 
             <Text style={styles.title}>Bem-vindo !</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>            
-        </View>    
+           
+            <FlatList
+                data={[1,2,3,4,5,6]}
+                style={styles.IncidentList}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={()=>(
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG</Text>
+                        <Text style={styles.incidentValue}>APAD</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR :</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+                        <TouchableOpacity 
+                            style={styles.detailsButton}
+                            onPress={navigateToDetail}
+                        >
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#E02041" />
+                        </TouchableOpacity>
+                    </View>                     
+                )}
+            />
+        </View>     
     );
 }
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -10,7 +10,7 @@ export default function Routes(){
     return(
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{ headerShown:false}}>
-                <AppStack.Screen name="Incidentes" component={Incidents} />
+                <AppStack.Screen name="Incidents" component={Incidents} />
                 <AppStack.Screen name="Detail" component={Detail} />
             </AppStack.Navigator>
         </NavigationContainer>
